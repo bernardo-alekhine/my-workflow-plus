06ef260e9f57e6903fb3825a0bc44278ed9a8636
bernardo-alekhine <alekhinebernardo@gmail.com>
2026-03-31 02:13:08 +0000

    fix: add health point for healthcheck

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("health/", lambda r: HttpResponse("OK")),
 ]
