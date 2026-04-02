04c7e10dbd2a5dfe0a3d0f04fa89635ad27bfcb9
bernardo-alekhine <alekhinebernardo@gmail.com>
2026-04-02 21:38:28 +0000

    feat: add address factory, shift meta inner class to be declared after main fields

--- backend/apps/users/tests/factories.py
+++ backend/apps/users/tests/factories.py
@@ -1,19 +1,37 @@
 import factory
-from apps.users.models import User, TaxRegistration
+from apps.users.models import User, TaxRegistration, Address
 
 
 class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Faker("user_name")
+
     class Meta:
         model = User
 
-    username = factory.Faker("user_name")
-
 
 class TaxRegistrationFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = TaxRegistration
-
     user = factory.SubFactory(UserFactory)
     country_code = "BR"
     id_type = "CNPJ"
     value = factory.Sequence(lambda n: f"123456780001{n:02d}")
+
+    class Meta:
+        model = TaxRegistration
+
+
+class AddressFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    address_type = factory.Iterator([choice[0] for choice in Address.Types.choices])
+    street = factory.Faker("street_address")
+    number = factory.Faker("building_number")
+    neighborhood = factory.Faker("street_name", locale="pt_BR")
+    line_2 = factory.Faker("secondary_address")
+    city = factory.Faker("city")
+    state = factory.Faker("state_abbr")
+    country = factory.Faker("country")
+    complement = factory.Faker("secondary_address")
+    postal_code = factory.Faker("postalcode")
+    is_default = factory.Iterator([False, True])
+
+    class Meta:
+        model = Address
